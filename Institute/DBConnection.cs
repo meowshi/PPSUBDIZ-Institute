a026ff9ba9f1a97d87abe87d24a884583d629144
meowshi <denlas31@gmail.com>
2021-12-11 17:58:55 +0300

    Добавил возмоность добавлять записи.

--- Institute/DBConnection.cs
+++ Institute/DBConnection.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,6 @@ namespace Institute
     {
         private static MySqlConnection _mySqlConnection = null;
         private static string _connectionString = "Server=141.8.192.151;Database=f0608526_ppsubdiz;port=3306;User Id=f0608526_ppsubdiz;password=ppsubdiz";
-        // private static string _connectionString = "SERVER=localhost;DATABASE=institute;UID=root;PASSWORD=d18594604";
 
         /// <summary>
         /// Подключение к базе данных. При исключении показывает MessageBox c сообщением из исключения.
@@ -27,6 +27,51 @@ namespace Institute
             {
                 MessageBox.Show("Не удалось подключиться к базе данных.", ex.Message);
             }
+        }
+
+        public static void AddData(string query)
+        {
+            try
+            {
+                _mySqlConnection.Open();
+
+                MySqlCommand command = new MySqlCommand(query, _mySqlConnection);
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Поздравляем!", "Запись успешно добавлена!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Не удалось добавить данные в базу.");
+            }
+            finally
+            {
+                _mySqlConnection.Close();
+            }
+        }
+
+        public static DataTable SelectData(string query)
+        {
+            DataTable table = null;
+            try
+            {
+                _mySqlConnection.Open();
+                MySqlCommand command = new MySqlCommand(query, _mySqlConnection);
+                command.ExecuteNonQuery();
+
+                table = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                adapter.Fill(table);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка получения данных");
+            }
+            finally
+            {
+                _mySqlConnection.Close();
+            }
+            return table;
         }
     }
 }
--- Institute/MainWindow.xaml.cs
+++ Institute/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,9 @@ namespace Institute
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
+        const string PassportColumns = "series,number,issue_date,expiry_date,issuing_authority";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +31,86 @@ namespace Institute
             DBConnection.Connect();
 
             DisableTabs();
+        }
+
+        public void NotImplemented()
+        {
+            MessageBox.Show("НЕ реализовал!");
+        }
+
+        private void ClearGrid(Grid grid)
+        {
+            var childs = LogicalTreeHelper.GetChildren(grid);
+            
+            foreach (var child in childs)
+            {
+                var textBox = child as TextBox;
+                if (textBox != null)
+                {
+                    textBox.Clear();
+                }
+                else
+                {
+                    var datePicker = child as DatePicker;
+                    if (datePicker != null)
+                    {
+                        datePicker.Text = String.Empty;
+                    }
+                }
+            }
+        }
+
+        private bool IsFieldsFilled(Grid grid)
+        {
+            var childs = LogicalTreeHelper.GetChildren(grid);
+
+            foreach (var child in childs)
+            {
+                if (child is TextBox textBox && textBox.Text.Equals(String.Empty))
+                {
+                    MessageBox.Show("Не все поля заполнены!", "Внимание!");
+                    return false;
+                }
+                else if (child is DatePicker datePicker && datePicker.Text.Equals(String.Empty))
+                {
+                    MessageBox.Show("Не все поля даты заполнены!", "Внимание!");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private string MakeAddQuery(string table, Grid grid, string columns)
+        {
+            var querySB = new StringBuilder($"insert into {table} ({columns}) values (");
+            
+            var childs = LogicalTreeHelper.GetChildren(grid);
+            foreach (var child in childs) {
+                if (child is TextBox textBox)
+                {
+                    querySB.Append($"'{textBox.Text}',");
+                    Trace.WriteLine(textBox.Text);
+                }
+                else if (child is DatePicker datePicker)
+                {
+                    querySB.Append($"'{datePicker.Text}',");
+                    Trace.WriteLine(datePicker.Text);
+                }
+            }
+
+            querySB.Remove(querySB.Length - 1, 1);
+            querySB.Append(')');
+
+            return querySB.ToString();
+        }
+
+        private void Add(string table, Grid grid, string columns = "")
+        {
+            if (!IsFieldsFilled(grid)) return;
+
+            string query = MakeAddQuery(table, grid, columns);
+            DBConnection.AddData(query);
         }
 
         /// <summary>
@@ -75,6 +158,152 @@ namespace Institute
             mainSubsystemTab.Visibility = Visibility.Collapsed;
             repotSubsystemTab.Visibility = Visibility.Collapsed;
             controlSubsystemTab.Visibility = Visibility.Collapsed;
+        }
+
+        private void butAddDepartment_Click(object sender, RoutedEventArgs e)
+        {
+            Add("department", gAddDepartment);
+        }
+
+        private void butClearAddDepartment_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddDepartment);
+        }
+
+        private void butAddFaculty_Click(object sender, RoutedEventArgs e)
+        {
+            Add("faculty", gAddFaculty);
+        }
+
+        private void butClearAddFaculty_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddFaculty);
+        }
+
+        private void butAddChair_Click(object sender, RoutedEventArgs e)
+        {
+            Add("chair", gAddChair);
+        }
+
+        private void butClearAddChair_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddChair);
+        }
+
+        private void butAddSpeciality_Click(object sender, RoutedEventArgs e)
+        {
+            Add("speciality", gAddSpeciality);
+        }
+
+        private void butClearAddSpeciality_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddSpeciality);
+        }
+
+        private void butAddDiscipline_Click(object sender, RoutedEventArgs e)
+        {
+            Add("discipline", gAddDiscipline);
+        }
+
+        private void butClearAddDiscipline_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddDiscipline);
+        }
+
+        private void butAddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsFieldsFilled(gAddEmployee)) return;
+
+            // Сначала добавляем паспорт так как надо получить код паспортных данных.
+            string passportValues = $"'{tbAddEmployeePassportSeries.Text}','{tbAddEmployeePassportNumber.Text}','{dpAddEmployeePassportIssueDate.Text}','{dpAddEmployeePassportExpiryDate.Text}','{tbAddEmployeePassportIssuingAuthority.Text}'";
+            string passportQuery = $"insert into passport_data ({PassportColumns}) values ({passportValues})";
+            DBConnection.AddData(passportQuery);
+            
+            var table = DBConnection.SelectData($"select id from passport_data where number = '{tbAddEmployeePassportNumber.Text}'");
+            string passportId = table.Rows[0][0].ToString();
+
+            string employeeColumns = "surname,name,patronymic,inn, phone_number,email,post,salary,department_name, passport_data_id";
+            string employeeValues = $"'{tbAddEmployeeSurname.Text}', '{tbAddEmployeeName.Text}', '{tbAddEmployeePatronymic.Text}' , '{tbAddEmployeeINN.Text}', '{tbAddEmployeePhoneNumber.Text}'," +
+                                     $"'{tbAddEmployeeEmail.Text}', '{tbAddEmployeePost.Text}', '{tbAddEmployeeSalary.Text}', '{tbAddEmployeeDepartmentName.Text}', '{passportId}'";
+            string employeeQuery = $"insert into employee ({employeeColumns}) values ({employeeValues})";
+            DBConnection.AddData(employeeQuery);
+        }
+
+        private void butClearAddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddEmployee);
+        }
+
+        private void butAddTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            Add("teacher", gAddTeacher);
+        }
+
+        private void butClearAddTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddTeacher);
+        }
+
+        private void butAddGroup_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsFieldsFilled(gAddGroup)) return;
+
+            string query = $"insert into `group` values ('{tbAddGroupName.Text}', '{tbAddGroupFacultyName.Text}')";
+            DBConnection.AddData(query);
+        }
+
+        private void butClearAddGroup_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddGroup);
+        }
+
+        private void butAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsFieldsFilled(gAddStudent)) return;
+
+            // Сначала добавляем паспорт так как надо получить код паспортных данных.
+            string passportValues = $"'{tbAddStudentPassportSeries.Text}','{tbAddStudentPassportNumber.Text}','{dpAddStudentPassportIssueDate.Text}','{dpAddStudentPassportExpiryDate.Text}','{tbAddStudentPassportIssuingAuthority.Text}'";
+            string passportQuery = $"insert into passport_data ({PassportColumns}) values ({passportValues})";
+            DBConnection.AddData(passportQuery);
+
+            var table = DBConnection.SelectData($"select id from passport_data where number = '{tbAddStudentPassportNumber.Text}'");
+            string passportId = table.Rows[0][0].ToString();
+
+            string studentColumns = "surname,name,patronymic,inn,phone_number,email,speciality_name,chair_name,group_name,start_date,end_date,education_cost,passport_data_id";
+            string studentValues = $"'{tbAddStudentSurname.Text}', '{tbAddStudentName.Text}', '{tbAddStudentPatronymic.Text}' , '{tbAddStudentINN.Text}', '{tbAddStudentPhoneNumber.Text}'," +
+                $"'{tbAddStudentEmail.Text}', '{tbAddStudentSpecialityName.Text}', '{tbAddStudentChairName.Text}', '{tbAddStudentGroupName.Text}', '{dpAddStudentStartDate.Text}'," +
+                $"'{dpAddStudentEndDate.Text}', '{tbAddStudentEduCost.Text}', '{passportId}'";
+            string studentQuery = $"insert into student ({studentColumns}) values ({studentValues})";
+            DBConnection.AddData(studentQuery);
+        }
+
+        private void butClearAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddStudent);
+        }
+
+        private void butAddEnrollee_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsFieldsFilled(gAddEnrollee)) return;
+
+            // Сначала добавляем паспорт так как надо получить код паспортных данных.
+            string passportValues = $"'{tbAddEnrolleePassportSeries.Text}','{tbAddEnrolleePassportNumber.Text}','{dpAddEnrolleePassportIssueDate.Text}','{dpAddEnrolleePassportExpiryDate.Text}','{tbAddEnrolleePassportIssuingAuthority.Text}'";
+            string passportQuery = $"insert into passport_data ({PassportColumns}) values ({passportValues})";
+            DBConnection.AddData(passportQuery);
+
+            var table = DBConnection.SelectData($"select id from passport_data where number = '{tbAddEnrolleePassportNumber.Text}'");
+            string passportId = table.Rows[0][0].ToString();
+
+            string enrolleeColumns = "surname,name,patronymic,document_type,total_score,passport_data_id";
+            string enrolleeValues = $"'{tbAddEnrolleeSurname.Text}','{tbAddEnrolleeName.Text}','{tbAddEnrolleePatronymic.Text}'," +
+                $"'{tbAddEnrolleeDocumentType.Text}','{tbAddEnrolleeTotalScore.Text}', '{passportId}'";
+            string enrolleeQuery = $"insert into enrollee ({enrolleeColumns}) values ({enrolleeValues})";
+            DBConnection.AddData(enrolleeQuery);
+        }
+
+        private void butClearAddEnrollee_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGrid(gAddEnrollee);
         }
     }
 }
