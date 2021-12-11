c5b51866974f9be1344cd85e5a590e97b16565a5
meowshi <denlas31@gmail.com>
2021-12-11 10:20:07 +0300

    Добавьте файлы проекта.

---
+++ Institute/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Institute
+{
+    internal static class DBConnection
+    {
+        private static MySqlConnection _mySqlConnection = null;
+        private static string _connectionString = "SERVER=localhost;DATABASE=institute;UID=root;PASSWORD=d18594604";
+
+        /// <summary>
+        /// Подключение к базе данных. При исключении показывает MessageBox c сообщением из исключения.
+        /// </summary>
+        public static void Connect()
+        {
+            try
+            {
+                _mySqlConnection = new MySqlConnection(_connectionString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось подключиться к базе данных.", ex.Message);
+            }
+        }
+    }
+}
---
+++ Institute/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Institute
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Подключаемся к базе данных.
+            DBConnection.Connect();
+
+            DisableTabs();
+        }
+
+        /// <summary>
+        /// Прячет все вкладки. Далее они будут включаться/отключаться нажатием вкладок меню.
+        /// </summary>
+        private void DisableTabs()
+        {
+            mainSubsystemTab.Visibility = Visibility.Collapsed;
+            repotSubsystemTab.Visibility = Visibility.Collapsed;
+            controlSubsystemTab.Visibility = Visibility.Collapsed;
+            feedbackSubsystemGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void mainSubsystemButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainSubsystemTab.Visibility = Visibility.Visible;
+
+            repotSubsystemTab.Visibility = Visibility.Collapsed;
+            controlSubsystemTab.Visibility = Visibility.Collapsed;
+            feedbackSubsystemGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void reportSubsystemButton_Click(object sender, RoutedEventArgs e)
+        {
+            repotSubsystemTab.Visibility = Visibility.Visible;
+
+            mainSubsystemTab.Visibility = Visibility.Collapsed;
+            controlSubsystemTab.Visibility = Visibility.Collapsed;
+            feedbackSubsystemGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void controlSubsystemButton_Click(object sender, RoutedEventArgs e)
+        {
+            controlSubsystemTab.Visibility = Visibility.Visible;
+
+            mainSubsystemTab.Visibility = Visibility.Collapsed;
+            repotSubsystemTab.Visibility = Visibility.Collapsed;
+            feedbackSubsystemGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void feedbackSubsystemButton_Click(object sender, RoutedEventArgs e)
+        {
+            feedbackSubsystemGrid.Visibility = Visibility.Visible;
+
+            mainSubsystemTab.Visibility = Visibility.Collapsed;
+            repotSubsystemTab.Visibility = Visibility.Collapsed;
+            controlSubsystemTab.Visibility = Visibility.Collapsed;
+        }
+    }
+}
