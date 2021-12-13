5814b18f0a929f8a48d7325575965006ea5769e6
meowshi <denlas31@gmail.com>
2021-12-13 15:37:55 +0300

    Начал делать разделение на уровни доступа.

---
+++ Institute/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Institute
+{
+    internal enum AccessLevel
+    {
+        Zero,
+        Minimal,
+        Teacher,
+        ChairHead,
+        FacultyHead,
+        Rector,
+        HR,
+        AdmissionCommittee,
+        Full
+    }
+
+    internal static class User
+    {
+        public static string Login { get; set; }
+        public static string Surname { get; set; }
+        public static string Name { get; set; }
+        public static string Patronymic { get; set; }
+        public static string PhoneNumber { get; set; }
+        public static string Email { get; set; }
+        public static AccessLevel AccessLevel { get; set; }
+
+        public static void SetAccessLevel()
+        {
+            ;
+        }
+    }
+}
